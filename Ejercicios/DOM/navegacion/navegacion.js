b378ce09abed1ddb1912e9773a4e0fe62ffbe77f
Herlan <herlanherbas@gmail.com>
2024-06-25 00:05:11 -0400

    add mainjs navigation dom

---
+++ Ejercicios/DOM/navegacion/navegacion.js
@@ -0,0 +1,6 @@
+const child = document.getElementById('child')
+const parent = child.parentElement;
+console.log('Parent element', parent)
+const firstChild = child.lastChild
+console.log(firstChild)
+
